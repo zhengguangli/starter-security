417658350a55bcc60e4f563b462ded705ea43151
lizhengguang <lizg10@asiainfo.com>
2021-03-24 19:42:47 +0800

    Add HelloT Controller

---
+++ src/main/java/wang/icopy/security/controller/HellowT.java
@@ -0,0 +1,13 @@
+package wang.icopy.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HellowT {
+
+    @GetMapping("/success")
+    public String test() {
+        return "success";
+    }
+}
